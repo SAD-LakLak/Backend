8403764b6c029759c4959750b6842b8ae3589ef8
Mohammadmahdi Farahani <fmohammadmahdi1381@gmail.com>
2024-12-29 10:20:38 +0330

    resolving merge

--- LakLak-Site/core/models.py
+++ LakLak-Site/core/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.conf import settings
+from datetime import timedelta
 
 # Create your models here.
 class Product(models.Model):
