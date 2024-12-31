2c4b7a00b96a3a079c1e0879c4cc295f08149788
Mohammadmahdi Farahani <fmohammadmahdi1381@gmail.com>
2024-12-31 12:04:56 +0330

    fixed user foreignkey for product

--- LakLak-Site/core/models.py
+++ LakLak-Site/core/models.py
@@ -7,39 +7,6 @@ from datetime import timedelta
 
 
 # Create your models here.
-class Product(models.Model):
-    TYPE_CHOICES = (
-        ('edible', 'Edible Products'),
-        ('clothing', 'Clothing'),
-        ('toiletries', 'Toiletries')
-    )
-    type = models.CharField(max_length=50, choices=TYPE_CHOICES, blank=True)
-    name = models.CharField(max_length=255, blank=True)
-    info = models.TextField(max_length=5000, blank=True)
-    is_active = models.BooleanField(default=False, blank=True)
-    is_deleted = models.BooleanField(default=False, blank=True)
-    last_update = models.DateTimeField(auto_now=True, blank=True)
-    creation_date = models.DateTimeField(auto_created=True, blank=True)
-    price = models.PositiveBigIntegerField(blank=True)
-    stock = models.PositiveIntegerField(blank=True)
-    images = models.ManyToManyField('ProductImage', blank=True, related_name='products')
-
-    groups = models.ManyToManyField(
-        Group,
-        related_name='customuser_groups',  # Ensure a unique related_name
-        blank=True,
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='customuser_permissions',  # Ensure a unique related_name
-        blank=True,
-    )
-
-
-class ProductImage(models.Model):
-    image = models.ImageField(upload_to='product_images/')
-    product = models.ForeignKey(Product, related_name='product_images', on_delete=models.CASCADE)
-
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
@@ -63,3 +30,37 @@ class PasswordRecoveryRequest(models.Model):
 
     def has_expired(self):
         return self.date_created < timezone.now() - timedelta(minutes=settings.EMAIL_REQUEST_TTL)
+
+class Product(models.Model):
+    TYPE_CHOICES = (
+        ('edible', 'Edible Products'),
+        ('clothing', 'Clothing'),
+        ('toiletries', 'Toiletries')
+    )
+    provider = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, blank=True)
+    name = models.CharField(max_length=255, blank=True)
+    info = models.TextField(max_length=5000, blank=True)
+    is_active = models.BooleanField(default=False, blank=True)
+    is_deleted = models.BooleanField(default=False, blank=True)
+    last_update = models.DateTimeField(auto_now=True, blank=True)
+    creation_date = models.DateTimeField(auto_created=True, blank=True)
+    price = models.PositiveBigIntegerField(blank=True)
+    stock = models.PositiveIntegerField(blank=True)
+    images = models.ManyToManyField('ProductImage', blank=True, related_name='products')
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_groups',  # Ensure a unique related_name
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions',  # Ensure a unique related_name
+        blank=True,
+    )
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+    product = models.ForeignKey(Product, related_name='product_images', on_delete=models.CASCADE)
+
