1ba08ea0002e829eff0645b86328d9e860c287a3
Moeein Aali <moeeeinaali@gmail.com>
2025-02-27 16:21:23 +0330

    Update settings.py

--- LakLak/settings.py
+++ LakLak/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = 'django-insecure-)%!=4d4d^2js&*vtgpj&hh1qnethb_8ze92da4@22!l71$6yj4
 DEBUG = True
 
 CORS_ALLOW_ALL_ORIGINS = True
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://api.laklakbox.ir',
+    'https://*.laklakbox.ir',  
+    'https://laklakbox.ir',  
+]
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '5.34.203.201','213.233.184.204','api.laklakbox.ir','*.laklakbox.ir']
 AUTH_USER_MODEL = 'core.CustomUser'
 
